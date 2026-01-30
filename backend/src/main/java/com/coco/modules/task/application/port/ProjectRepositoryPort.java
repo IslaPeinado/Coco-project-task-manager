bc35bf5cdd0fbaeadeb859cd4cb1ca25f29835d4
IslaDev <i.peinadohenriquez@gmail.com>
2026-01-30 18:33:51 +0100

    FIX

--- backend/src/main/java/com/coco/modules/task/application/port/ProjectRepositoryPort.java
+++
@@ -1,9 +0,0 @@
-package com.coco.modules.task.application.port;
-
-import java.util.Optional;
-
-public interface ProjectRepositoryPort {
-
-    // Obtener un proyecto por su ID
-    Optional<Project> findById(Long projectId);
-}
