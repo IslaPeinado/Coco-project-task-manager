636a059cbf4eb8f9af3b1538b19968010b0e8aeb
IslaDev <i.peinadohenriquez@gmail.com>
2026-01-30 12:38:47 +0100

    FIX / task "get method"

--- backend/src/main/java/com/coco/modules/task/api/TaskController.java
+++ backend/src/main/java/com/coco/modules/task/api/TaskController.java
@@ -2,6 +2,7 @@ package com.coco.modules.task.api;
 
 import com.coco.modules.task.api.dto.TaskCreateRequest;
 import com.coco.modules.task.application.CreateTaskUseCase;
+import com.coco.modules.task.application.GetTaskUseCase;
 import com.coco.modules.task.domain.Task;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -11,9 +12,11 @@ import org.springframework.web.bind.annotation.*;
 public class TaskController {
 
     private final CreateTaskUseCase createTaskUseCase;
+    private final GetTaskUseCase getTaskUseCase;
 
-    public TaskController(CreateTaskUseCase createTaskUseCase) {
+    public TaskController(CreateTaskUseCase createTaskUseCase, GetTaskUseCase getTaskUseCase) {
         this.createTaskUseCase = createTaskUseCase;
+        this.getTaskUseCase = getTaskUseCase;
     }
 
     //CREATE
