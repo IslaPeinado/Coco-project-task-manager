cac5092838b5d8a96e17f1cb1d6e3d66a993e1e0
IslaDev <i.peinadohenriquez@gmail.com>
2026-03-09 11:12:28 +0100

    FEATURE / enviroment config

--- frontend/src/app/core/services/auth/auth.service.ts
+++ frontend/src/app/core/services/auth/auth.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  
+  readonly apiBaseUrl = environment.API_BASE_URL;
+  readonly loginEndpoint = `${this.apiBaseUrl}/auth/login`;
 }
