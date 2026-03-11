31ed66561ca1f7d1b2ae6f4355c440de2100a6b7
IslaDev <i.peinadohenriquez@gmail.com>
2026-03-11 11:01:22 +0100

    FEATURE / shared config for Button Input Card Badge

---
+++ frontend/src/app/shared/shared.config.ts
@@ -0,0 +1,4 @@
+export { Badge } from './components/badge/badge';
+export { Button } from './components/button/button';
+export { Card } from './components/card/card';
+export { CocoInput } from './components/input/coco-input';
