df6713262121127d3c12cc5b4daa3518e31b0602
IslaDev <i.peinadohenriquez@gmail.com>
2026-03-16 15:25:19 +0100

    FEATURE / automaizaciones de test y de documentaciones

--- frontend/src/app/features/projects/projects.routes.ts
+++
@@ -1,9 +0,0 @@
-﻿import { Routes } from '@angular/router';
-import { ProjectsPageComponent } from './pages/projects-page.component';
-
-export const PROJECTS_ROUTES: Routes = [
-  {
-    path: '',
-    component: ProjectsPageComponent,
-  },
-];
--- frontend/src/app/features/users/users.routes.ts
+++
@@ -1,9 +0,0 @@
-﻿import { Routes } from '@angular/router';
-import { UsersPageComponent } from './pages/users-page.component';
-
-export const USERS_ROUTES: Routes = [
-  {
-    path: '',
-    component: UsersPageComponent,
-  },
-];
